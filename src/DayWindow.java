22bf779181068423202362cdc893900370486a46
mattcmerritt <mattcmerritt@gmail.com>
2021-04-10 13:46:17 -0400

    fixed multiple bugs when returning to day window from input window

--- src/DayWindow.java
+++ src/DayWindow.java
@@ -123,15 +123,82 @@ public class DayWindow extends JFrame {
             }
             this.setVisible(true);
         }
-        else {
+        else if (calendar.getDay(day) != null) {
             // need to regenerate entire panel
-        	// TODO: figure out why panel is flickering for a frame
         	this.remove(mainPanel);
         	this.remove(bottomPanel);
         	this.remove(insidePanel);
         	this.remove(heading);
-        	this.dispose();
-        	loadDay(day);
+
+            heading = new JLabel("Day " + day, SwingConstants.CENTER);
+            heading.setFont(new Font(heading.getFont().getName(), Font.PLAIN, 24));
+
+            mainPanel = new JPanel(new BorderLayout());
+            bottomPanel = new JPanel();
+
+            backButton = new JButton("Back to Month");
+
+            backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // close this window or hide it
+                    monthWindow.reload();
+                    DayWindow.this.dispose();
+                }
+            });
+
+            questions = new JLabel[WaterUsage.QUESTIONS.length];
+            for (int i = 0; i < questions.length; i++) {
+                questions[i] = new JLabel(WaterUsage.QUESTIONS[i]);
+            }
+
+            inputFields = new JTextField[questions.length];
+            for (int i = 0; i < inputFields.length; i++) {
+                inputFields[i] = new JTextField(10);
+                inputFields[i].setEditable(false);
+                inputFields[i].setText("" + calendar.getDay(day).getAnswer(i));
+            }
+
+            gallonLabels = new JLabel[questions.length];
+            for (int i = 0; i < gallonLabels.length; i++) {
+                gallonLabels[i] = new JLabel("Gallons used: " + ((int) ((Double.parseDouble(inputFields[i].getText()) * WaterUsage.DEFAULT_MULTIPLIERS[i]) * 10) / 10.0)  + " gallons");
+            }
+
+            insidePanel = new JPanel(new GridLayout(questions.length, 3, 5, 5));
+            for (int i = 0; i < questions.length && i < inputFields.length; i++) {
+                insidePanel.add(questions[i]);
+                insidePanel.add(inputFields[i]);
+                insidePanel.add(gallonLabels[i]);
+            }
+            changeButton = new JButton("Overwrite Data");
+
+            changeButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    inputWindow = new InputWindow(calendar);
+                    inputWindow.addDayWindow(DayWindow.this);
+                    inputWindow.loadDay(day);
+                    DayWindow.this.setVisible(false);
+                }
+            });
+
+            bottomPanel.add(changeButton);
+            bottomPanel.add(backButton);
+
+            mainPanel.add(insidePanel, BorderLayout.CENTER);
+            mainPanel.add(heading, BorderLayout.NORTH);
+            mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+            // spacing
+            mainPanel.add(new JPanel(), BorderLayout.WEST);
+            mainPanel.add(new JPanel(), BorderLayout.EAST);
+
+            this.add(mainPanel);
+            this.pack();
+            this.setVisible(true);
+        }
+        else {
+            this.setVisible(true);
         }
         
     }
